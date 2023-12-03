80f8b7b4e27c82590ac61295d7c463cada65dfb0
Vincenzo Sanzone <vincenzo.sanzone02@gmail.com>
2023-12-03 17:15:38 +0100

    Solution to problem of day two part two

--- 2023/day/two/CubeConundrum.cpp
+++ 2023/day/two/CubeConundrum.cpp
@@ -1,3 +1,42 @@
 //
-// Created by vsanz on 03/12/2023.
+// Created by Vincenzo Sanzone on 03/12/2023.
 //
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	string thisLine;
+	cin >> thisLine;
+	int sum = 0;
+	while (!thisLine.empty()) {
+		cin >> thisLine >> thisLine;
+		int actualBlue, actualGreen, actualRed;
+		int atLeastBlue, atLeastGreen, atLeastRed;
+		actualBlue = actualGreen = actualRed = atLeastBlue = atLeastGreen = atLeastRed = 0;
+		while (thisLine != "Game" && thisLine != "STOP") {
+			int value = stoi(thisLine.substr(0, thisLine.size()));
+			cin >> thisLine;
+			if (thisLine.substr(0, thisLine.size() - 1) == "blue" || thisLine == "blue") {
+				actualBlue += value;
+			} else if (thisLine.substr(0, thisLine.size() - 1) == "green" || thisLine == "green") {
+				actualGreen += value;
+			} else if (thisLine.substr(0, thisLine.size() - 1) == "red" || thisLine == "red") {
+				actualRed += value;
+			}
+			if (thisLine.substr(thisLine.size() - 1, thisLine.size()) != ",") {
+				atLeastBlue = max(actualBlue, atLeastBlue);
+				atLeastGreen = max(actualGreen, atLeastGreen);
+				atLeastRed = max(actualRed, atLeastRed);
+				actualBlue = actualGreen = actualRed = 0;
+			}
+			cin >> thisLine;
+		}
+		sum += atLeastBlue * atLeastGreen * atLeastRed;
+		if (thisLine == "STOP") {
+			break;
+		}
+	}
+	cout << sum << endl;
+}
