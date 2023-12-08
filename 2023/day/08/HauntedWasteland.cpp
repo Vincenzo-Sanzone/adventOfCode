6c3da220d3494323b963b4c9b1a8b70952d26704
Vincenzo Sanzone <vincenzo.sanzone02@gmail.com>
2023-12-08 16:52:03 +0100

    Solution day 8 part 2

--- 2023/day/08/HauntedWasteland.cpp
+++ 2023/day/08/HauntedWasteland.cpp
@@ -8,30 +8,61 @@ using namespace std;
 typedef long long ll;
 typedef pair<string, string> pss;
 
-int main(){
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+	return (a / gcd(a, b)) * b;
+}
+
+ll lcm(vector<ll> number) {
+	ll result = number[0];
+	for (ll i = 1; i < number.size(); i++) {
+		result = lcm(result, number[i]);
+	}
+	return result;
+}
+
+int main() {
 	string map;
 	cin >> map;
 	unordered_map<string, pss> instructions;
 	string keyName;
+	vector<string> actualPosition;
 	while (cin >> keyName && keyName != "STOP") {
+		if (keyName[keyName.size() - 1] == 'A') {
+			actualPosition.push_back(keyName);
+		}
 		string equals, left, right;
 		cin >> equals >> left >> right;
-		instructions[keyName] = pss(left.substr(1, left.size()-2), right.substr(0, right.size()-1));
+		instructions[keyName] = pss(left.substr(1, left.size() - 2), right.substr(0, right.size() - 1));
 	}
-	ll pass = 0;
+	vector<ll> pass(actualPosition.size());
 	ll actualInstruction = 0;
-	string actualPosition = "AAA";
-	while(actualPosition != "ZZZ"){
-		pss movies= instructions[actualPosition];
-		if(map[actualInstruction] == 'L'){
-			actualPosition = movies.first;
+	for (ll i = 0; i < actualPosition.size(); i++) {
+		bool readAll = false;
+		bool foundZ = false;
+		while (actualPosition[i][actualPosition[i].size() - 1] != 'Z' || !readAll ||
+		       !foundZ) {
+			string newPosition;
+			if (map[actualInstruction] == 'L') {
+				newPosition = instructions[actualPosition[i]].first;
+
+			} else {
+				newPosition = instructions[actualPosition[i]].second;
+			}
+			actualPosition[i] = newPosition;
+			pass[i]++;
+			actualInstruction++;
+			if (actualInstruction == map.size()) {
+				actualInstruction = 0;
+				readAll = true;
+			}
+			if (newPosition[newPosition.size() - 1] == 'Z') foundZ = true;
 		}
-		else{
-			actualPosition = movies.second;
-		}
-		pass++;
-		actualInstruction++;
-		if(actualInstruction == map.size()) actualInstruction = 0;
 	}
-	cout << pass << endl;
+	cout << lcm(pass) << endl;
 }
